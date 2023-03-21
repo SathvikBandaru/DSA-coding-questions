e905ffde56a48fca89e1c131d29132fb06ac6388
SathvikBandaru <106403308+SathvikBandaru@users.noreply.github.com>
2023-03-21 21:19:19 +0530

    Create Print multiplication table.cpp

---
+++ HackerRank/Print multiplication table.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=10;i++){
+        cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+    }
+    
+    return 0;
+}
