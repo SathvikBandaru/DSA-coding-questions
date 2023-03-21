2fe2728a1595df645216d80e7d3a4f834d318726
SathvikBandaru <106403308+SathvikBandaru@users.noreply.github.com>
2023-03-21 21:15:28 +0530

    Create Cubes sum.cpp

---
+++ HackerRank/Cubes sum.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    long long a=pow((n*(n+1))/2,2);
+        cout<<a<<endl;
+    
+    return 0;
+}
