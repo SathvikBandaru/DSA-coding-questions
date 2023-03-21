5aa3467201da93437ae15e1b4167a2018fb72314
SathvikBandaru <106403308+SathvikBandaru@users.noreply.github.com>
2023-03-21 20:35:09 +0530

    Create Transpose matrix.cpp

---
+++ HackerRank/Transpose matrix.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    int a[n][m],b[m][n];
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            b[j][i]=a[i][j];
+        }
+    }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<b[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
